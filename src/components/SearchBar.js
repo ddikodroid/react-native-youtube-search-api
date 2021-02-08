81dfad7df61975fb678c2678872ac8e38787b875
ddikodroid <ddikodroid@gmail.com>
2021-02-08 09:41:56 +0700

    add searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {COLOR} from '../styles/Color';
+import {WIDTH, HEIGHT, FONT} from '../styles/Dimension';
+const SearchBar = (props) => {
+  return (
+    <View style={styles.searchBarContainer}>
+      <TextInput
+        style={styles.textInputField}
+        onChangeText={props.onChangeText}
+        placeholder={props.placeholder}
+      />
+      <TouchableOpacity style={styles.submitField} onPress={props.onPress}>
+        <Text style={FONT.inputFieldText}>Cari</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  searchBarContainer: {
+    width: WIDTH * 0.95,
+    height: HEIGHT * 0.055,
+    borderRadius: WIDTH * 0.02,
+    marginVertical: HEIGHT * 0.01,
+    backgroundColor: COLOR.gray,
+    justifyContent: 'center',
+    paddingLeft: WIDTH * 0.025,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  submitField: {
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    width: WIDTH * 0.15,
+    height: HEIGHT * 0.055,
+    alignItems: 'center',
+    flexBasis: '25%',
+  },
+  textInputField: {
+    flexBasis: '75%',
+    ...FONT.inputFieldText,
+  },
+});
