79fc1ced6942ef6b2d633a96fcf13fbaa33ce34f
ddikodroid <ddikodroid@gmail.com>
2021-02-08 09:21:49 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
